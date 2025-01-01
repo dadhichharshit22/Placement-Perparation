c9bb9bbbadb70ecb80c89b4fd506c688bdee2557
HARSHIT DADHICH <136362586+dadhichharshit22@users.noreply.github.com>
2025-01-01 12:27:25 +0530

    Add files via upload

---
+++ Loop/reverseNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int reverseNumber(int number){
+    return number;
+}
+
+int main(){
+    // Make a number variable
+    int number;
+   // now take input  from the user
+    cin>>number;
+  // function 
+    cout<<reverseNumber(number);
+
+    return 0;
+}
